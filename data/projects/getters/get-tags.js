41d6da82783df43ffa8db93dd639da1ed35bdeb4
calvin.kipperman@gmail.com <emnudge@gmail.com>
2019-11-21 18:37:53 -0500

    moved from .flat() to a reducer to fix netlify deploy errors

--- data/projects/getters/get-tags.js
+++ data/projects/getters/get-tags.js
@@ -1,6 +1,8 @@
 const projects = require("./get-projects.js");
 
-const tagsWithDups = projects.map(project => project.tags).flat();
+const tagsWithDups = projects
+  .map(project => project.tags)
+  .reduce((accum, curr) => [...accum, ...curr], []);
 
 const tagsNoDups = Array.from(new Set(tagsWithDups));
 
