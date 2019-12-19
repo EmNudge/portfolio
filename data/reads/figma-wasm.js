55dc94c72195ad6b5d6a56c9b4864d2006b5244f
emnudge <emnudge@gmail.com>
2019-12-19 09:24:29 -0500

    added 2 more articles

---
+++ data/reads/figma-wasm.js
@@ -0,0 +1,8 @@
+export const meta = {
+  author: "Evan Wallace",
+  title: "WebAssembly cut Figma's load time by 3x",
+  url: "https://www.figma.com/blog/webassembly-cut-figmas-load-time-by-3x/",
+  description: "An interesting look at the current applications of wasm to speed up web applications.",
+  readAt: "December 18, 2019",
+  createdAt: "June 8, 2017"
+};
---
+++ data/reads/rust-iot.js
@@ -0,0 +1,8 @@
+export const meta = {
+  author: "James Munns",
+  title: "From zero to main(): Bare metal Rust",
+  url: "https://interrupt.memfault.com/blog/zero-to-main-rust-1",
+  description: "An interesting look in Rust's application as a language for extremely low level hardware.",
+  readAt: "December 17th, 2019",
+  createdAt: "December 17th, 2019"
+};
