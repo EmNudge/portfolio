6924bf94818575c44319600f38b93335aed0ed56
emnudge <emnudge@gmail.com>
2019-12-19 09:41:00 -0500

    fixed dates

--- data/reads/rust-iot.js
+++ data/reads/rust-iot.js
@@ -3,6 +3,6 @@ export const meta = {
   title: "From zero to main(): Bare metal Rust",
   url: "https://interrupt.memfault.com/blog/zero-to-main-rust-1",
   description: "An interesting look in Rust's application as a language for extremely low level hardware.",
-  readAt: "December 17th, 2019",
-  createdAt: "December 17th, 2019"
+  readAt: "December 17, 2019",
+  createdAt: "December 17, 2019"
 };
