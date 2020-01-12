04d2e4ea4987dac5c1ff79b0725641dccaad65b5
emnudge <emnudge@gmail.com>
2020-01-12 03:14:20 -0500

    organized projects section and added some side spacing for mobile

--- pages/projects.js
+++ pages/projects.js
@@ -1,47 +1,41 @@
-import Layout from "../components/layouts/Main";
-import Searchbox from "../components/Searchbox";
-import { projects, tags } from "../data/projects";
-import ProjectBanner from "../components/ProjectBanner";
+import Layout from '../components/layouts/Main';
+import Searchbox from '../components/Searchbox';
+import { projects, tags } from '../data/projects';
+import ProjectsContainer from '../components/ProjectsContainer';
 
 const Projects = () => {
-  const [searchTags, setSearchTags] = React.useState([]);
+	const [searchTags, setSearchTags] = React.useState([]);
 
-  const [rerenderForcer, setRerenderForcer] = React.useState(1);
+	const [rerenderForcer, setRerenderForcer] = React.useState(1);
 
-  const addTag = tag => {
-    if (searchTags.includes(tag)) return;
-    setSearchTags([...searchTags, tag]);
-  };
+	const addTag = (tag) => {
+		if (searchTags.includes(tag)) return;
+		setSearchTags([...searchTags, tag]);
+	};
 
-  const getFilteredProjects = () => {
-    if (!searchTags.length) return projects;
+	const getFilteredProjects = () => {
+		if (!searchTags.length) return projects;
 
-    return projects.filter(project =>
-      searchTags.every(tag => project.tags.includes(tag))
-    );
-  };
+		return projects.filter((project) => searchTags.every((tag) => project.tags.includes(tag)));
+	};
 
-  const handleChange = tag => {
-    setRerenderForcer(rerenderForcer * -1);
-    setSearchTags(tag);
-  };
+	const handleChange = (tag) => {
+		setRerenderForcer(rerenderForcer * -1);
+		setSearchTags(tag);
+	};
 
-  return (
-    <Layout title="Projects">
-      <Searchbox tags={tags} addedTags={searchTags} onChange={handleChange} />
-      <div className="projects-container" key={rerenderForcer}>
-        {getFilteredProjects().map((project, index) => (
-          <ProjectBanner
-            animationDelay={`.${index * 2}s`}
-            onTagClick={addTag}
-            key={project.title}
-            selectedTags={searchTags}
-            {...project}
-          />
-        ))}
-      </div>
-    </Layout>
-  );
+	return (
+		<Layout title="Projects">
+			<Searchbox tags={tags} addedTags={searchTags} onChange={handleChange} />
+
+			<ProjectsContainer
+				key={rerenderForcer}
+				projects={getFilteredProjects()}
+				selectedTags={searchTags}
+				onTagClick={addTag}
+			/>
+		</Layout>
+	);
 };
 
 export default Projects;
