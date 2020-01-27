773c56b600400cda273c9cc87d4ac05fe49c4241
EmNudge <emnudge@gmail.com>
2020-01-27 13:19:01 -0500

    added a read

---
+++ data/reads/rust-parser-combinator.js
@@ -0,0 +1,8 @@
+export const meta = {
+  author: "Bodil Stokke",
+  title: "Learning Parser Combinators With Rust",
+  url: "https://bodil.lol/parser-combinators/",
+  description: "Condensed and fantastic tutorial.",
+  readAt: "January 26, 2020",
+  createdAt: "April 18, 2019"
+};
